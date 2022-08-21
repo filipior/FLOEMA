999a7c688c35e82a8cb899a282f414e69c3ecc73
Filip Banasiak <filip.banasiak@avt.pl>
2022-08-22 00:10:05 +0200

    pug structure

--- app.js
+++ app.js
@@ -7,7 +7,19 @@ app.set('views', path.join(__dirname, 'views'))
 app.set('view engine', 'pug')
 
 app.get('/', (req, res) => {
-  res.render('index')
+  res.render('pages/home')
+})
+
+app.get('/about', (req, res) => {
+  res.render('pages/about')
+})
+
+app.get('/collections/', (req, res) => {
+  res.render('pages/collections')
+})
+
+app.get('/detail/:id', (req, res) => {
+  res.render('pages/detail')
 })
 
 app.listen(port, () => {
