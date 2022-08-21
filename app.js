4845ea9906151c1d1daff16bab1e6fbbb7113250
Filip Banasiak <filip.banasiak@avt.pl>
2022-08-21 23:26:56 +0200

    pug

--- app.js
+++ app.js
@@ -1,9 +1,13 @@
 const express = require('express')
 const app = express()
+const path = require('path')
 const port = 3000
 
+app.set('views', path.join(__dirname, 'views'))
+app.set('view engine', 'pug')
+
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+  res.render('index')
 })
 
 app.listen(port, () => {
