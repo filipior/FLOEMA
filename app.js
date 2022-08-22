d3b09b16e7261d2f489fd272ed04a1b82ea93a78
Filip Banasiak <filip.banasiak@avt.pl>
2022-08-22 19:30:31 +0200

    update to prismic v.6

--- app.js
+++ app.js
@@ -6,33 +6,132 @@ const path = require('path')
 const port = 3000
 
 const Prismic = require('@prismicio/client')
-const PrismicDOM = require('prismic-dom')
+const PrismicH = require('@prismicio/helpers')
 
-// const handleLinkResolver = (doc) => {
-//   if (doc.type === 'page') return `/${doc.lang}/${doc.uid}`
-//   if (doc.type === 'homepage') return `/${doc.lang}`
-//   return '/'
-// }
+// Initialize the prismic.io api
 
-app.set('views', path.join(__dirname, 'views'))
+const initApi = (req) => {
+  return Prismic.createClient(process.env.PRISMIC_ENDPOINT, {
+    accessToken: process.env.PRISMIC_ACCESS_TOKEN,
+    req,
+    fetch
+  })
+}
+
+// Link Resolver
+
+const HandleLinkResolver = (doc) => {
+  // Define the url depending on the document type
+  //   if (doc.type === 'page') {
+  //     return '/page/' + doc.uid;
+  //   } else if (doc.type === 'blog_post') {
+  //     return '/blog/' + doc.uid;
+  //   }
+
+  // Default to homepage
+  return '/'
+}
+
+// Middleware
+
+app.use((req, res, next) => {
+  res.locals.ctx = {
+    endpoint: process.env.PRISMIC_ENDPOINT,
+    linkResolver: HandleLinkResolver
+  }
+  res.locals.PrismicH = PrismicH
+
+  next()
+})
+
 app.set('view engine', 'pug')
+app.set('views', path.join(__dirname, 'views'))
+app.locals.basedir = app.get('views')
 
-app.get('/', (req, res) => {
-  res.render('pages/home')
+const handleRequest = async (api) => {
+  const [meta, home, about, { results: collections }] = await Promise.all([
+    api.getSingle('meta'),
+    api.getSingle('home'),
+    api.getSingle('about'),
+    api.query(Prismic.Predicates.at('document.type', 'collection'), {
+      fetchLinks: 'product.image'
+    })
+  ])
+
+  const assets = []
+
+  home.data.gallery.forEach((item) => {
+    assets.push(item.image.url)
+  })
+
+  about.data.gallery.forEach((item) => {
+    assets.push(item.image.url)
+  })
+
+  about.data.body.forEach((section) => {
+    if (section.slice_type === 'gallery') {
+      section.items.forEach((item) => {
+        assets.push(item.image.url)
+      })
+    }
+  })
+
+  collections.forEach((collection) => {
+    collection.data.products.forEach((item) => {
+      assets.push(item.products_product.data.image.url)
+    })
+  })
+
+  return {
+    assets,
+    meta,
+    home,
+    collections,
+    about
+  }
+}
+
+app.get('/', async (req, res) => {
+  const api = await initApi(req)
+  const defaults = await handleRequest(api)
+
+  res.render('pages/home', {
+    ...defaults
+  })
 })
 
-app.get('/about', (req, res) => {
-  res.render('pages/about')
+app.get('/about', async (req, res) => {
+  const api = await initApi(req)
+  const defaults = await handleRequest(api)
+
+  res.render('pages/about', {
+    ...defaults
+  })
 })
 
-app.get('/collections/', (req, res) => {
-  res.render('pages/collections')
+app.get('/collections', async (req, res) => {
+  const api = await initApi(req)
+  const defaults = await handleRequest(api)
+
+  res.render('pages/collections', {
+    ...defaults
+  })
 })
 
-app.get('/detail/:uid', (req, res) => {
-  res.render('pages/detail')
+app.get('/detail/:uid', async (req, res) => {
+  const api = await initApi(req)
+  const defaults = await handleRequest(api)
+
+  const product = await api.getByUID('product', req.params.uid, {
+    fetchLinks: 'collection.title'
+  })
+
+  res.render('pages/detail', {
+    ...defaults,
+    product
+  })
 })
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
+  console.log(`Example app listening at http://localhost:${port}`)
 })
