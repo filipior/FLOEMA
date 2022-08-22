997aca4b6bff9c56712bf67d028ac769f0818b39
Filip Banasiak <filip.banasiak@avt.pl>
2022-08-22 18:14:46 +0200

    prismic start

--- app.js
+++ app.js
@@ -1,7 +1,18 @@
+require('dotenv').config()
+
 const express = require('express')
 const app = express()
 const path = require('path')
 const port = 3000
+
+const Prismic = require('@prismicio/client')
+const PrismicDOM = require('prismic-dom')
+
+// const handleLinkResolver = (doc) => {
+//   if (doc.type === 'page') return `/${doc.lang}/${doc.uid}`
+//   if (doc.type === 'homepage') return `/${doc.lang}`
+//   return '/'
+// }
 
 app.set('views', path.join(__dirname, 'views'))
 app.set('view engine', 'pug')
@@ -18,7 +29,7 @@ app.get('/collections/', (req, res) => {
   res.render('pages/collections')
 })
 
-app.get('/detail/:id', (req, res) => {
+app.get('/detail/:uid', (req, res) => {
   res.render('pages/detail')
 })
 
