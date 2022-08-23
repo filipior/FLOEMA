2090f82bee15dd290c2c88597ec6dcdfd54d79f6
Filip Banasiak <filip.banasiak@avt.pl>
2022-08-23 23:18:18 +0200

    collection, navigation pug

--- app.js
+++ app.js
@@ -1,14 +1,27 @@
 require('dotenv').config()
 
 const fetch = require('node-fetch')
+const logger = require('morgan')
 const path = require('path')
 const express = require('express')
+const errorHandler = require('errorhandler')
+const bodyParser = require('body-parser')
+const methodOverride = require('method-override')
 
 const app = express()
-const port = process.env.PORT || 3000
+const port = process.env.PORT || 8004
 
 const Prismic = require('@prismicio/client')
 const PrismicH = require('@prismicio/helpers')
+const { application } = require('express')
+const UAParser = require('ua-parser-js')
+
+app.use(logger('dev'))
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(errorHandler())
+app.use(methodOverride())
+app.use(express.static(path.join(__dirname, 'public')))
 
 // Initialize the prismic.io api
 const initApi = (req) => {
@@ -21,12 +34,17 @@ const initApi = (req) => {
 
 // Link Resolver
 const HandleLinkResolver = (doc) => {
-  // Define the url depending on the document type
-  //   if (doc.type === 'page') {
-  //     return '/page/' + doc.uid;
-  //   } else if (doc.type === 'blog_post') {
-  //     return '/blog/' + doc.uid;
-  //   }
+  if (doc.type === 'product') {
+    return `/detail/${doc.slug}`
+  }
+
+  if (doc.type === 'collections') {
+    return '/collections'
+  }
+
+  if (doc.type === 'about') {
+    return '/about'
+  }
 
   // Default to homepage
   return '/'
@@ -34,11 +52,25 @@ const HandleLinkResolver = (doc) => {
 
 // Middleware to inject prismic context
 app.use((req, res, next) => {
-  res.locals.ctx = {
-    endpoint: process.env.PRISMIC_ENDPOINT,
-    linkResolver: HandleLinkResolver
-  }
+  const ua = UAParser(req.headers['user-agent'])
+
+  res.locals.isDesktop = ua.device.type === undefined
+  res.locals.isPhone = ua.device.type === 'mobile'
+  res.locals.isTablet = ua.device.type === 'tablet'
+
+  res.locals.Link = HandleLinkResolver
   res.locals.PrismicH = PrismicH
+  res.locals.Numbers = (index) => {
+    return index === 0
+      ? 'One'
+      : index === 1
+        ? 'Two'
+        : index === 2
+          ? 'Three'
+          : index === 3
+            ? 'Four'
+            : ''
+  }
 
   next()
 })
@@ -48,16 +80,27 @@ app.set('views', path.join(__dirname, 'views'))
 app.locals.basedir = app.get('views')
 
 const handleRequest = async (api) => {
-  const [meta, home, about, { results: collections }] = await Promise.all([
-    api.getSingle('meta'),
-    api.getSingle('home'),
-    api.getSingle('about'),
-    api.query(Prismic.Predicates.at('document.type', 'collection'), {
-      fetchLinks: 'product.image'
-    })
-  ])
+  const [meta, preloader, navigation, home, about, { results: collections }] =
+    await Promise.all([
+      api.getSingle('meta'),
+      api.getSingle('preloader'),
+      api.getSingle('navigation'),
+      api.getSingle('home'),
+      api.getSingle('about'),
+      api.query(Prismic.Predicates.at('document.type', 'collection'), {
+        fetchLinks: 'product.image'
+      })
+    ])
 
   const assets = []
+
+  home.data.gallery.forEach((item) => {
+    assets.push(item.image.url)
+  })
+
+  about.data.gallery.forEach((item) => {
+    assets.push(item.image.url)
+  })
 
   about.data.body.forEach((section) => {
     if (section.slice_type === 'gallery') {
@@ -67,12 +110,22 @@ const handleRequest = async (api) => {
     }
   })
 
+  collections.forEach((collection) => {
+    collection.data.products.forEach((item) => {
+      assets.push(item.products_product.data.image.url)
+    })
+  })
+
+  console.log(assets)
+
   return {
     assets,
     meta,
     home,
     collections,
-    about
+    about,
+    navigation,
+    preloader
   }
 }
 
